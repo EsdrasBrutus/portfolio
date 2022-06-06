5e9b11aec4ed344714005be9f170455856c08d5d
GenHero <genhero@hotmail.com>
2022-06-06 18:27:52 -0400

    intial fil structure for frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,17 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
-
+import { About, Footer, Skills, Testimonial, Works, Header } from './components/containers';
+import { NavBar } from './components';
 function App() {
   return (
     <div className="App">
-     
+      <NavBar />
+      <Header />
+      <About />
+      <Skills />
+      <Works />
+      <Testimonial />
+      <Footer />
     </div>
   );
 }
